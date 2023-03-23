a282f4aad02b5d3fe1df5ef61572b3b610c8521d
Jesus Morales <53502658+jgmorale@users.noreply.github.com>
2023-03-23 15:40:13 -0600

    Add personalized content to home and help page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   root "application#hello"
 end
